05b60281d9a161cd2bfff81538516474e32df016
Andy <andy.zheng.couto@gmail.com>
2026-03-06 18:44:00 -0500

    Biggest 32 bit int

---
+++ big_number.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    unsigned int a = 2000000000;
+    unsigned int newA = a;
+    newA = a+a;
+
+    cout << newA << endl;
+
+    // while(a > 0)
+    // {
+    //     a += 1000000;
+    //     cout << a << endl;
+    // }
+
+    // newA = a;
+       
+    // while(newA > 0)
+    // {
+    //     a = newA;
+    //     newA += 100000;
+    //     cout << a << endl;
+    // }
+
+    // newA = a;
+
+    // while(newA > 0)
+    // {
+    //     a = newA;
+    //     newA += 10000;
+    //     cout << a << endl;
+    // }
+
+    // newA = a;
+
+    // while(newA > 0)
+    // {
+    //     a = newA;
+    //     newA += 1000;
+    //     cout << a << endl;
+    // }
+
+    // newA = a;
+
+    //     while(newA > 0)
+    // {
+    //     a = newA;
+    //     newA += 1;
+    //     cout << a << endl;
+    // }
+
+    // newA = a;
+
+   
+ 
+    return 0;
+}
