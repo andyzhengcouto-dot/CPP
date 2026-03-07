bca40dcd04c2942b6984e4188899f3be9a0b92e7
Andy <andy.zheng.couto@gmail.com>
2026-03-07 08:25:17 -0500

    Base template for the problem

---
+++ Problem1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int y;
+    
+    cout << "Insert youngest below:" << endl;
+    
+    cin >> y;
+    
+    int m = y + 5;
+    int o = m + 5;
+
+    
+    
+    cout << "Youngest Child:" << y << endl;
+    cout << "Middle Child:" << m << endl;
+    cout << "Oldest Child:" << o << endl;
+ 
+ 
+    return 0;
+}
