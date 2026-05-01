7b901c33602a15d5879d3701d1ea3cf292b08731
Andy <andy.zheng.couto@gmail.com>
2026-05-01 19:00:20 -0400

    Andy's Projects Update! Updated outputs and duplicates deleted!

--- 2026/Andy's Projects/Idk.cpp
+++ 2026/Andy's Projects/Idk.cpp
@@ -3,7 +3,7 @@ using namespace std;
  
 int main() 
 {
-    cout << ":)" << endl;
+    cout << " Smile!" << ":)" << endl;
  
     return 0;
 }
--- 2026/Andy's Projects/Sitting BUnny.cpp
+++ 2026/Andy's Projects/Sitting BUnny.cpp
@@ -6,7 +6,6 @@ int main() {
     cout << "( . .)" << endl;
     cout << "C('')('')" << endl;
     cout << "Sitting Bunny" << endl;
-    cout << "By:Andy" << endl;
  
  
     return 0;
